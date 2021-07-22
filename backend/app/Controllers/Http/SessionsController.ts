c63c80155de878d838ee0b6e678801b4c6913075
gstimm <contato.gabrieltimm@gmail.com>
2021-07-22 11:19:07 -0300

    feat: add session validation

--- backend/app/Controllers/Http/SessionsController.ts
+++ backend/app/Controllers/Http/SessionsController.ts
@@ -1,27 +1,34 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Hash from '@ioc:Adonis/Core/Hash'
 import User from '../../Models/User'
+import Session from 'App/Validators/SessionValidator'
 
 export default class SessionsController {
   public async store({ request, response, auth }: HttpContextContract) {
-    const { email, password } = request.only(['email', 'password'])
+    await request.validate(Session)
 
-    const user = await User.findBy('email', email)
+    try {
+      const { email, password } = request.only(['email', 'password'])
 
-    if (!user) {
-      return response.status(404).json({ message: 'User not found' })
+      const user = await User.findBy('email', email)
+
+      if (!user) {
+        return response.status(404).json({ message: 'User not found' })
+      }
+
+      const isValid = await Hash.verify(user.password, password)
+
+      if (!isValid) {
+        return response.status(403).json({ message: 'Email or password incorrect.' })
+      }
+
+      const token = await auth.use('api').generate(user, {
+        expiresIn: '60mins',
+      })
+
+      return token
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: err.message } })
     }
-
-    const isValid = await Hash.verify(user.password, password)
-
-    if (!isValid) {
-      return response.json({ message: 'Incorrect password' })
-    }
-
-    const token = await auth.use('api').generate(user, {
-      expiresIn: '60mins',
-    })
-
-    return token
   }
 }
