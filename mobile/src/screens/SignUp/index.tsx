8a7c99ff3d4d3e12c1209031d7aa3c2bbc084d59
gstimm <contato.gabrieltimm@gmail.com>
2021-08-16 15:22:39 -0300

    feat: add SignUp logic

--- mobile/src/screens/SignUp/index.tsx
+++ mobile/src/screens/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { UnAuthStackList } from '../../routes/UnAuthRoutes';
 import { StackNavigationProp } from '@react-navigation/stack';
@@ -6,6 +6,14 @@ import { Logo, Input, Card, Button, Footer } from '../../components/index';
 import { Container, Title } from './styles';
 import colors from '../../utils/colors';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SignUpSchema } from '../../utils/schemas';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { useSelector } from 'react-redux';
+import { ApplicationStore } from '../../store';
+import { AuthState } from '../../store/ducks/auth';
+import api from '../../services/api';
+import { ScrollView } from 'react-native';
 
 type LoginScreenNavigationProp = StackNavigationProp<UnAuthStackList, 'SignUp'>
 
@@ -13,22 +21,126 @@ interface NavProps {
   navigation: LoginScreenNavigationProp;
 }
 
+interface SignUpForm {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
 const SignUpScreen: React.FC<NavProps> = ({ navigation }) => {
+  const { error } = useSelector<ApplicationStore, AuthState>(
+    state => state.Auth,
+  );
+
+  const {
+    handleSubmit,
+    control,
+    register,
+    formState: { errors }
+  } = useForm<SignUpForm>({ resolver: yupResolver(SignUpSchema) })
+
+  useEffect(() => {
+    register('name'),
+      register('email')
+    register('password')
+    register('password_confirmation')
+  }, [register])
+
+  useEffect(() => {
+    if (error) {
+      alert(error);
+    }
+  }, [error]);
+
+
+  const onSubmit: SubmitHandler<SignUpForm> = async data => {
+    if (Object.keys(errors).length) {
+      alert('Please fill all fields.');
+      return;
+    }
+
+    try {
+      await api.post('/users', data);
+      alert('User created successfully.');
+      navigation.navigate('Login');
+    } catch (error) {
+      error.response.data.errors.map((err: { message: string }) =>
+        alert(err.message),
+      );
+    }
+  }
+
   return (
-    <>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+    >
       <Container>
         <Logo />
         <Title>Registration</Title>
         <Card>
-          <Input type='text' label='Name' value={''} onChangeText={() => console.log('dsad')} />
-          <Input type='email' label='Email' value={''} onChangeText={() => console.log('dsad')} />
-          <Input type='password' label='Password' value={''} onChangeText={() => console.log('dsad')} />
+          <Controller
+            control={control}
+            name='name'
+            defaultValue=''
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type='text'
+                label='Name'
+                value={value}
+                onChangeText={onChange}
+                error={errors.name?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name='email'
+            defaultValue=''
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type='email'
+                label='Email'
+                value={value}
+                onChangeText={onChange}
+                error={errors.email?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name='password'
+            defaultValue=''
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type='password'
+                label='Password'
+                value={value}
+                onChangeText={onChange}
+                error={errors.password?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name='password_confirmation'
+            defaultValue=''
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type='password'
+                label='Password Confirmation'
+                value={value}
+                onChangeText={onChange}
+                error={errors.password_confirmation?.message}
+              />
+            )}
+          />
           <Button
             color={colors.lightGreen}
             title="Register"
             iconSide="right"
             style={{ marginTop: 21, marginBottom: 30 }}
-            onPress={() => navigation.push('Login')}
+            onPress={handleSubmit(onSubmit)}
           >
             <MaterialCommunityIcons
               name="arrow-right"
@@ -52,7 +164,7 @@ const SignUpScreen: React.FC<NavProps> = ({ navigation }) => {
         </Button>
       </Container>
       <Footer />
-    </>
+    </ScrollView>
   );
 };
 
